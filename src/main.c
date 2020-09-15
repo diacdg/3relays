4820f35f7e1fd8be128ab594502a9a739073b5d5
Doru Diaconescu <diacdg@gmail.com>
2020-09-15 18:18:15 +0300

    Added device status api endpoint

--- src/main.c
+++ src/main.c
@@ -16,14 +16,18 @@
  */
 
 #include "stdio.h"
+#include "string.h"
 #include "mgos.h"
 #include "mgos_http_server.h"
+#include "common/json_utils.h"
+#include "common/str_util.h"
 
 #define LED_PIN 16
 #define HIGH 1
 #define LOW 0
 #define RELAYS_COUNT 3
 #define MGOS_F_RELOAD_CONFIG MG_F_USER_5
+#define JSON_HEADERS "Connection: close\r\nContent-Type: application/json"
 #define LISTENER_SPEC "80"
 
 int relaysPinMap[RELAYS_COUNT] = {12,13,14};
@@ -49,6 +53,42 @@ static void set_off_relay_handler(struct mg_connection *c, int ev, void *p, void
 	mgos_gpio_write(relaysPinMap[*((int*)relay_index)], LOW);
   
   	sendRelayStatusResponse(c, *((int*)relay_index), mgos_gpio_read(relaysPinMap[*((int*)relay_index)]));
+}
+
+static void relay_status_handler(struct mg_connection *c, int ev, void *p, void *relay_index) {
+	if (ev != MG_EV_HTTP_REQUEST) return;
+	  
+  	sendRelayStatusResponse(c, *((int*)relay_index), mgos_gpio_read(relaysPinMap[*((int*)relay_index)]));
+}
+
+static void device_status_handler(struct mg_connection *c, int ev, void *p, void *user_data) {
+	if (ev != MG_EV_HTTP_REQUEST) return;
+	
+	int i;
+	struct mbuf jsmb;
+  	struct json_out jsout = JSON_OUT_MBUF(&jsmb);
+  	mbuf_init(&jsmb, 0);
+	
+	json_printf(&jsout, "{uptime: %.2lf,ram: %lu, free_ram: %lu, relays: [", mgos_uptime(), (unsigned long) mgos_get_heap_size(), (unsigned long) mgos_get_free_heap_size());
+	
+	for (i=0; i<RELAYS_COUNT; i++) {
+		json_printf(&jsout, "{relay: %d, status: %d}", i+1, mgos_gpio_read(relaysPinMap[i]));
+		
+		if (RELAYS_COUNT-1 != i) {
+			json_printf(&jsout, ",");
+		}
+	}
+	
+	json_printf(&jsout, "]}");
+	  
+  	if (jsmb.len > 0) {
+    		mg_send_head(c, 200, jsmb.len, JSON_HEADERS);
+    		mg_send(c, jsmb.buf, jsmb.len);
+  	}
+  	c->flags |= MG_F_SEND_AND_CLOSE;
+  	mbuf_free(&jsmb);
+  	
+  	(void) user_data;
 }
 
 
@@ -84,7 +124,12 @@ for (i=0; i<RELAYS_COUNT; i++) {
 	
 	sprintf(path, "/relay/%d/off", i+1);
 	mgos_register_http_endpoint(path, set_off_relay_handler, relayIndex);
+	
+	sprintf(path, "/relay/%d/status", i+1);
+	mgos_register_http_endpoint(path, relay_status_handler, relayIndex);
 }
+
+mgos_register_http_endpoint("/status", device_status_handler, NULL);
 
 #ifdef LED_PIN
   mgos_gpio_setup_output(LED_PIN, 0);
